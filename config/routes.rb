45689730aeef26dab63890bf9f5c5b4ba8d8c51a
Juan Carlos Gonzalez Petrocchi <juancarlosgzz23@gmail.com>
2022-05-06 19:47:23 -0500

    Add routes, run migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :v1 do 
+    resources :schools, except: %i[index] do
+      resources :recipients
+      resources :orders do
+        put :ship
+      end
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,63 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_05_06_224856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "gifts", force: :cascade do |t|
+    t.integer "type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "order_gifts", force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.bigint "gift_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["gift_id"], name: "index_order_gifts_on_gift_id"
+    t.index ["order_id"], name: "index_order_gifts_on_order_id"
+  end
+
+  create_table "order_recipients", force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.bigint "recipient_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_order_recipients_on_order_id"
+    t.index ["recipient_id"], name: "index_order_recipients_on_recipient_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "school_id", null: false
+    t.integer "workflow_status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["school_id"], name: "index_orders_on_school_id"
+  end
+
+  create_table "recipients", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.bigint "school_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["school_id"], name: "index_recipients_on_school_id"
+  end
+
+  create_table "schools", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "order_gifts", "gifts"
+  add_foreign_key "order_gifts", "orders"
+  add_foreign_key "order_recipients", "orders"
+  add_foreign_key "order_recipients", "recipients"
+  add_foreign_key "orders", "schools"
+  add_foreign_key "recipients", "schools"
 end
